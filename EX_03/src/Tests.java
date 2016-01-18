01117e449da8207dc279d8965a31603bdc5e4cd5
Aneta Ochedowska <aneta.ochedowska@gmail.com>
2016-01-18 14:28:31 +0100

    Changes to be committed: 	new file:   EX_03/.idea/compiler.xml 	new file:   EX_03/.idea/copyright/profiles_settings.xml 	new file:   EX_03/.idea/description.html 	new file:   EX_03/.idea/encodings.xml 	new file:   EX_03/.idea/misc.xml 	new file:   EX_03/.idea/modules.xml 	new file:   EX_03/.idea/workspace.xml 	new file:   EX_03/EX_03.iml 	new file:   EX_03/out/production/EX_03/Main.class 	new file:   EX_03/out/production/EX_03/Tests.class 	new file:   EX_03/src/Main.java 	new file:   EX_03/src/Tests.java

---
+++ EX_03/src/Tests.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Aneta on 2016-01-18.
+ */
+public class Tests {
+    public void TestEmptyListCreation()
+    {
+
+        // Becouse I can catch assert errors in two ways
+        // I will do this in a shorter way
+    }
+}
