d5e0caed937ccc2674f0e65449d845c9fc58c70a
Aneta Ochedowska <aneta.ochedowska@gmail.com>
2016-01-19 18:28:54 +0100

    Changes to be committed: 	new file:   EX_02/.vs/EX_02/v14/.suo 	new file:   EX_02/EX_02.sln 	new file:   EX_02/EX_02/App.config 	new file:   EX_02/EX_02/EX_02.csproj 	new file:   EX_02/EX_02/EnumeratorOrder.cs 	new file:   EX_02/EX_02/Program.cs 	new file:   EX_02/EX_02/Properties/AssemblyInfo.cs 	new file:   EX_02/EX_02/Tree.cs 	new file:   EX_02/EX_02/bin/Debug/EX_02.exe 	new file:   EX_02/EX_02/bin/Debug/EX_02.exe.config new file:   EX_02/EX_02/bin/Debug/EX_02.pdb 	new file:   EX_02/EX_02/bin/Debug/EX_02.vshost.exe 	new file:   EX_02/EX_02/bin/Debug/EX_02.vshost.exe.config 	new file:   EX_02/EX_02/bin/Debug/EX_02.vshost.exe.manifest 	new file:   EX_02/EX_02/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

---
+++ EX_02/EX_02/EnumeratorOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX_02
+{
+    public enum EnumeratorOrder
+    {
+        /// <summary>
+        /// http://en.wikipedia.org/wiki/Breadth-first_search
+        /// </summary>
+        BreadthFirstSearch = 1,
+        /// <summary>
+        /// http://en.wikipedia.org/wiki/Depth-first_search
+        /// </summary>
+        DepthFirstSearch = 2
+    }
+}
---
+++ EX_02/EX_02/Tree.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using EX_02;
+using System.Collections.ObjectModel;
+
+
+
+public class Tree<T> : IEnumerable<T>
+{
+    private EnumeratorOrder order;
+    public EnumeratorOrder Order
+    {
+        get { return order; }
+        set
+        {
+            if(!Enum.IsDefined(typeof(EnumeratorOrder), value))
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            order = value;
+            foreach(var c in Children)
+            {
+                c.Order = value;
+            }
+        }
+    }
+
+    private T val;
+    public T Value
+    {
+        get { return val; }
+        set { val = value; }
+    }
+    
+    private List<Tree<T>> children;
+    public ReadOnlyCollection<Tree<T>> Children
+    {
+        get
+        {
+            return new ReadOnlyCollection<Tree<T>>(children.AsReadOnly());
+        }
+    }
+    
+    public Tree(T val, EnumeratorOrder order)
+    {
+        this.val = val;
+        this.order = order;
+
+        children = new List<Tree<T>>();
+    }
+
+    void BreadthFirstSearch(List<T> node)
+    {
+        foreach(var c in Children)
+        {
+            node.Add(c.Value);
+        }
+        foreach(var c in Children)
+        {
+            c.BreadthFirstSearch(node);
+        }
+    }
+
+    void DepthFirstSearch(List<T> node)
+    {
+        node.Add(Value);
+        foreach(var c in Children)
+        {
+            c.DepthFirstSearch(node);
+        }
+    }
+
+    public void Add(T val)
+    {
+        Tree<T> t = new Tree<T>(val, Order);
+        children.Add(t);
+    }
+
+    public void Add(Tree<T> subtree)
+    {
+        subtree.Order = Order;
+        children.Add(subtree);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var node = new List<T>();
+        switch (Order)
+        {
+            case EnumeratorOrder.BreadthFirstSearch:
+                node.Add(Value);
+                BreadthFirstSearch(node);
+                break;
+            case EnumeratorOrder.DepthFirstSearch:
+                DepthFirstSearch(node);
+                break;
+            //default:
+                //return GetEnumerator();
+        }
+
+        ReadOnlyCollection<T> il = new ReadOnlyCollection<T>(node);
+        foreach(var i in il)
+        {
+            yield return i;
+        }
+        
+        //return retNode.AsReadOnly().GetEnumerator();
+        //return node.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
